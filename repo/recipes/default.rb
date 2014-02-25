5a25a87594fead3d14393cfa71d407cef7abc865
Angelo San Ramon <AngeloSanRamon@gmail.com>
2014-02-25 13:52:41 -0800

    update repo and base cookbooks

--- repo/recipes/default.rb
+++ repo/recipes/default.rb
@@ -9,23 +9,27 @@
 package "rsync"
 package "mod_ssl"
 package "httpd"
+package "createrepo"
 
 directory "/repo/centos/6.5" do
 	owner "root"
 	group "root"
 	action :create
+	not_if {File.directory?("/repo/centos/6.5")}
 end
 
 directory "/var/www/html/repo" do
 	owner "root"
 	group "root"
 	action :create
+	not_if {File.directory?("/var/www/html/repo")}
 end
 
 link "/repo/centos/6" do
 	to "/repo/centos/6.5"
 	group "root"
 	owner "root"
+	not_if {File.symlink?("/repo/centos/6")}
 end
 
 link "/var/www/html/centos" do
@@ -38,6 +42,20 @@ cron "sync_with_mirror" do
 	command "rsync -avHPS rsync://mirrors.rit.edu/centos/6.5/ /repo/centos/6.5/"
 	weekday 0
 	hour 23
+end
+
+%w[ /repo/centos/custom /repo/centos/custom/RPMS ].each do |path|
+	directory path do
+		owner "root"
+		group "root"
+		mode 755
+		action :create
+		not_if {File.directory?(path)}
+	end
+end
+
+execute "createrepo" do
+	command "createrepo /repo/centos/custom"
 end
 
 cookbook_file "/etc/pki/tls/certs/ca.crt" do
@@ -68,13 +86,4 @@ end
 service "httpd" do
 	action [:enable,:start]
 end
-
-#template "/etc/sysconfig/iptables" do
-#	source "iptables.erb"
-#	notifies :restart,"service[iptables]"
-#end
-
-#service "iptables" do
-#	action :nothing
-#end
 
